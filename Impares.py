abda3edeef2dc04821ab961f12651377965f2275
david1267 <62625257+david1267@users.noreply.github.com>
2020-04-10 10:36:53 -0300

    Add files via upload

---
+++ Impares.py
@@ -0,0 +1,5 @@
+x = int(input())
+i = 1
+for x in range(i,x):
+    print(i)
+    i = i + 2
---
+++ Tabuada.py
@@ -0,0 +1,4 @@
+tabuada=int(input( ))
+
+for count in range(10):
+    print("%d x %d = %d" % (count+1, tabuada, tabuada*(count+1)) )
